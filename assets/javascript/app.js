97a888c7a1977d7cc578157af9395a2b2b0002a7
Huy <kennyt995@gmail.com>
2017-12-20 20:58:22 -0700

    push button will generate park in CO

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,12 +10,10 @@ $(document).ready(function () {
     };
     firebase.initializeApp(config);
 
-    console.log("working");
-
-    function fetchRequest() {
+    function fetchNPS(topic, location) {
 
         let key = 'ZKLb9xO0SnI4KkfXFdoM9fmLuFkJqtfVtXKPpxM0';
-        let url = 'https://cors-anywhere.herokuapp.com/https://developer.nps.gov/api/v1/campgrounds';
+        let url = 'https://cors-anywhere.herokuapp.com/https://developer.nps.gov/api/v1/' + topic + '?stateCode=co';
 
         var myHeaders = new Headers();
         myHeaders.append('X-Api-Key', key);
@@ -30,10 +28,30 @@ $(document).ready(function () {
             return response.json();
         }).then(function (json) {
             console.log(json);
+            $('.display-result').html(JSON.stringify(json));
         });
     }
 
-    fetchRequest();
+
+    // function initMap() {
+    //     var uluru = {
+    //         lat: -25.363,
+    //         lng: 131.044
+    //     };
+    //     var map = new google.maps.Map(document.getElementById('map'), {
+    //         zoom: 4,
+    //         center: uluru
+    //     });
+    //     var marker = new google.maps.Marker({
+    //         position: uluru,
+    //         map: map
+    //     });
+    // }
+
+    $('#getLocation').click(function () {
+        let location = $('#locationInput').val();
+        fetchNPS('parks', location);
+    })
 });
 
 //https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=ZKLb9xO0SnI4KkfXFdoM9fmLuFkJqtfVtXKPpxM0
