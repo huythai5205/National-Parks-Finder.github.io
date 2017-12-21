1edde5334762f701bd13ef8028fed2b9cd79ce5c
Huy <kennyt995@gmail.com>
2017-12-21 13:27:22 -0700

    added google map

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,10 +10,10 @@ $(document).ready(function () {
     };
     firebase.initializeApp(config);
 
-    function fetchNPS(topic, location) {
+    function fetchNPS(topic, string) {
 
         let key = 'ZKLb9xO0SnI4KkfXFdoM9fmLuFkJqtfVtXKPpxM0';
-        let url = 'https://cors-anywhere.herokuapp.com/https://developer.nps.gov/api/v1/' + topic + '?stateCode=co';
+        let url = 'https://cors-anywhere.herokuapp.com/https://developer.nps.gov/api/v1/' + topic + '' + string;
 
         var myHeaders = new Headers();
         myHeaders.append('X-Api-Key', key);
@@ -33,25 +33,31 @@ $(document).ready(function () {
     }
 
 
-    // function initMap() {
-    //     var uluru = {
-    //         lat: -25.363,
-    //         lng: 131.044
-    //     };
-    //     var map = new google.maps.Map(document.getElementById('map'), {
-    //         zoom: 4,
-    //         center: uluru
-    //     });
-    //     var marker = new google.maps.Marker({
-    //         position: uluru,
-    //         map: map
-    //     });
-    // }
+    function initMap() {
+        console.log("map working");
+        var uluru = {
+            lat: -25.363,
+            lng: 131.044
+        };
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+
+    }
+    initMap();
+
 
     $('#getLocation').click(function () {
-        let location = $('#locationInput').val();
+        let location = "?stateCode=" + $('#locationInput').val();
         fetchNPS('parks', location);
     })
+
+
 });
 
 //https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=ZKLb9xO0SnI4KkfXFdoM9fmLuFkJqtfVtXKPpxM0
