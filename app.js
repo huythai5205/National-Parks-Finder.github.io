df40f5f46733fd0675a1459ff05fc97c9fa6dc69
unknown <a.colmenero2992gmail.com>
2018-01-06 12:42:00 -0700

    Second Edit

--- app.js
+++ app.js
@@ -1,21 +1,73 @@
-let slideIndex =0;
-carousel();
 
-function carousel(){
+
+//second slide
+
+let slideIndex2 =0;
+carouselTwo();
+
+function carouselTwo(){
     let i;
-    let x = document.getElementsByClassName("mySlides");
+    let x = document.getElementsByClassName("featured1");
 
 
 for (i=0; i <x.length; i++){
     x[i].style.display = "none";
 };
 
-slideIndex++;
-if (slideIndex > x.length)
+slideIndex2++;
+if (slideIndex2 > x.length)
 {
-    slideIndex = 1
+    slideIndex2 = 1
 };
 
-x[slideIndex-1].style.display = "block";
-setTimeout(carousel, 2000);
+x[slideIndex2-1].style.display = "block";
+setTimeout(carouselTwo, 2000);
+};
+
+// //third slide
+
+let slideIndex3 =0;
+carouselThree();
+
+function carouselThree(){
+    let i;
+    let x = document.getElementsByClassName("featured2");
+
+
+for (i=0; i <x.length; i++){
+    x[i].style.display = "none";
+};
+
+slideIndex3++;
+if (slideIndex3 > x.length)
+{
+    slideIndex3 = 1
+};
+
+x[slideIndex3-1].style.display = "block";
+setTimeout(carouselThree, 2000);
+};
+
+// //fourth slide
+
+let slideIndex4 =0;
+carouselFour();
+
+function carouselFour(){
+    let i;
+    let x = document.getElementsByClassName("featured3");
+
+
+for (i=0; i <x.length; i++){
+    x[i].style.display = "none";
+};
+
+slideIndex4++;
+if (slideIndex4 > x.length)
+{
+    slideIndex4 = 1
+};
+
+x[slideIndex4-1].style.display = "block";
+setTimeout(carouselFour, 2000);
 };
